6dd0a2f54eafe52e6fb03361b31931edad25a283
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-03-23 05:59:55 +0100

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.buttons > button');
+const span = document.querySelector('span')
+const submit = document.querySelector('.sub')
+const ratings = document.querySelector('.rating')
+const thanks = document.querySelector('.thanks')
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        span.innerHTML = this.innerHTML;
+    });
+});
+
+submit.addEventListener('click', () => {
+    ratings.style.display = 'none';
+    thanks.style.display = 'block';
+})
